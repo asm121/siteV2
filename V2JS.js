34447e7640ae2b216c6c2b877ef1397d506fa26f
Austin <asm121@pitt.edu>
2017-05-29 23:29:29 -0400

    waves

--- V2JS.js
+++ V2JS.js
@@ -175,4 +175,22 @@ $(document).ready(function(){
         document.getElementById('#intro').style.height = "";
     }
 
+    //waves https://codepen.io/loktar00/pen/kfrKC was referenced
+    var ocean = document.getElementById("ocean"),
+        waveWidth = 5,
+        waveCount = Math.floor(window.innerWidth/waveWidth),
+        docFrag = document.createDocumentFragment();
+
+    for(var i = 0; i < waveCount; i++){
+        var wave = document.createElement("div");
+        wave.className += " wave";
+        docFrag.appendChild(wave);
+        wave.style.left = i * waveWidth + "px";
+        wave.style.webkitAnimationDelay = (i/100) + "s";
+    }
+
+    ocean.appendChild(docFrag);
+
+
+
 });
